7c5e94f667d1a238e3edc0582b95e5998e960b86
Valerie Andy <valdaraa@protonmail.com>
2022-11-15 09:01:40 -0500

    -

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,34 @@
 import React from 'react'
-import Layout from '@theme/Layout'
+import { Layout } from '@theme/Layout'
 import styles from './index.module.css'
-import HomepageLogo from '../../static/img/homepageLogo.svg'
 import { GettingStartedCards } from '../components/GettingStartedCards'
+import { ToolsCards } from '../components/ToolsCards'
+import { ShaderComponent } from '../components/ShaderComponent'
+import ProjectsCards from '../components/ProjectsCards'
+import HomepageLogo from '../../static/img/homepageLogo.svg'
+
 
 export default function Home() {
-    return (
-      <Layout>
-        <div>
-          <HomepageLogo className={styles.logoText} />
-          <h2 className={styles.headerSubtitle}>Create What's Missing</h2>
-        </div>
-        <main className={styles.layout}>
-          <div className={styles.layoutFlexBox}>
-            <div className={styles.gettingStartedFlexBox}>
-              <p className={styles.gettingStarted}>Getting Started</p>
-            </div>
-            <GettingStartedCards />
+  return (
+    <Layout>
+      <ShaderComponent />
+      <div>
+        <HomepageLogo className={styles.logoText} />
+        <h2 className={styles.headerSubtitle}>Create What's Missing</h2>
+      </div>
+      <main className={styles.layout}>
+        <div className={styles.layoutFlexBox}>
+          <div className={styles.gettingStartedFlexBox}>
+            <p className={styles.gettingStarted}>Getting Started</p>
           </div>
-        </main>
-      </Layout>
-    )
-  }
+          <GettingStartedCards />
+          <p className={styles.tools}>Tools</p>
+          <ToolsCards />
+          <p className={styles.projects}>Projects Building on ZORA</p>
+          <ProjectsCards />
+        </div>
+      </main>
+    </Layout>
+  )
+}
+
